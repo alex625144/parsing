c5d02b8005c4203da3bdf2123eaf353271302753
alex625144 <benechka1@gmail.com>
2023-03-12 23:09:54 +0200

    create entities.

--- src/main/java/com/parsing/repository/LotPDFResultRepository.java
+++ src/main/java/com/parsing/repository/LotPDFResultRepository.java
@@ -2,7 +2,6 @@ package com.parsing.repository;
 
 import com.parsing.model.LotPDFResult;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.UUID;
 
 public interface LotPDFResultRepository extends JpaRepository<LotPDFResult, UUID> {
--- src/main/java/com/parsing/repository/LotRepository.java
+++ src/main/java/com/parsing/repository/LotRepository.java
@@ -2,7 +2,6 @@ package com.parsing.repository;
 
 import com.parsing.model.Lot;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.UUID;
 
 public interface LotRepository extends JpaRepository<Lot, UUID> {
