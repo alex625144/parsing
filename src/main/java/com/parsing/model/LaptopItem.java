1e33a674737bebd0ff7d3d290042a94fc98ae705
alex625144 <benechka1@gmail.com>
2023-03-25 12:27:04 +0200

    fix fields entity.

--- src/main/java/com/parsing/model/LaptopItem.java
+++ src/main/java/com/parsing/model/LaptopItem.java
@@ -12,6 +12,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
+
 import java.math.BigDecimal;
 import java.util.UUID;
 
@@ -20,6 +22,7 @@ import java.util.UUID;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString(exclude = "lotPDFResult")
 @Table(name = "laptop_item")
 public class LaptopItem {
 
