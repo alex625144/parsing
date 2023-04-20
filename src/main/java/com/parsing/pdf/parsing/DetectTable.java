658b909de444820d793d7a686015b245ba4b06fa
alex625144 <benechka1@gmail.com>
2023-04-20 16:17:46 +0300

    create private method

--- src/main/java/com/parsing/pdf/parsing/DetectTable.java
+++ src/main/java/com/parsing/pdf/parsing/DetectTable.java
@@ -12,78 +12,66 @@ import org.opencv.imgproc.Imgproc;
 @Slf4j
 public class DetectTable {
 
+    private static double x1 = 500;
+    private static double y1 = 0;
+    private static double x2 = 0;
+    private static double y2 = 0;
+    private static double y3 = 1500;
+
     public static String detectTable(String fileSource) {
         OpenCV.loadLocally();
-        Mat dst = new Mat(), cdst = new Mat(), cdstP;
+        Mat dst = new Mat(), cdst = new Mat(), cdstP, cropTable = new Mat();
+        Mat source = Imgcodecs.imread(fileSource, Imgcodecs.IMREAD_GRAYSCALE);
 
-        // Load an image
-        Mat src = Imgcodecs.imread(fileSource, Imgcodecs.IMREAD_GRAYSCALE);
-
-        // Check if image is loaded fine
-        if (src.empty()) {
-            System.out.println("Error opening image!");
-            System.out.println("Program Arguments: [image_name -- default "
+        if (source.empty()) {
+            log.warn("Error opening image!");
+            log.warn("Program Arguments: [image_name -- default "
                     + fileSource + "] \n");
             System.exit(-1);
         }
 
-        // Edge detection
-        Imgproc.Canny(src, dst, 50, 200, 3, false);
+        Imgproc.Canny(source, dst, 50, 200, 3, false);
         Imgcodecs.imwrite("afterCanny.png", dst);
-
-        // Copy edges to the images that will display the results in BGR
         Imgproc.cvtColor(dst, cdst, Imgproc.COLOR_GRAY2BGR);
         cdstP = cdst.clone();
+        Mat linesP = new Mat();
+        Imgproc.HoughLinesP(dst, linesP, 1, Math.PI / 180, 25, 700, 10);
 
-
-        // Probabilistic Line Transform
-        Mat linesP = new Mat(); // will hold the results of the detection
-        Imgproc.HoughLinesP(dst, linesP, 1, Math.PI / 180, 25, 700, 10); // runs the actual detection
-
-        // Draw the lines and search Points
-        double xTopLeft = 500;
-        double yTopLeft = 0;
-        double xTopRight = 0;
-        double yTopRight = 0;
-        double xBottom = 0;
-        double yBottom = 1500;
-        System.out.println("lines rows = " + linesP.rows());
         for (int x = 0; x < linesP.rows(); x++) {
             double[] l = linesP.get(x, 0);
-            log.info(l[0] + " " + l[1] + " " + l[2] + " " + l[3]);
             Imgproc.line(cdstP, new Point(l[0], l[1]), new Point(l[2], l[3]), new Scalar(0, 0, 255), 3, Imgproc.LINE_AA, 0);
-            if(l[0]<xTopLeft){
-                xTopLeft= l[0];
+            if (l[1] > y1) {
+                y1 = l[1];
+                x2 = l[2];
+                y2 = l[3];
             }
-
-            if (l[1]>yTopLeft) {
-                yTopLeft = l[1];
-                xTopRight = l[2];
-                yTopRight = l[3];
+            if (l[1] < y3) {
+                y3 = l[1];
             }
-            if (l[1]<yBottom) {
-                yBottom = l[1];
+            if (l[2] > x2) {
+                x2 = l[2];
+            }
+            if (l[0] < x1) {
+                x1 = l[0];
             }
         }
-        xBottom=xTopLeft;
-        xTopLeft = 0;
+        Imgcodecs.imwrite("afterHough.png", cdstP);
 
-        //Look for height and width rectangle
-        double width = Math.sqrt((xTopRight - xTopLeft) * (xTopRight - xTopLeft) + (yTopRight - yTopLeft) * (yTopRight - yTopLeft));
-        double heigth = Math.sqrt((xTopLeft - xTopLeft) * (xTopLeft - xTopLeft) + (yBottom - yTopLeft) * (yBottom - yTopLeft));
-        //create Rectangle
-        final Rect rectangle = new Rect((int) xBottom, (int) yBottom, (int) width, (int) heigth);
-        //crop image
-        Mat sub = new Mat();
+        Rect rectangle = createRect(x1, y1, x2, y2, y3);
         try {
-            sub = src.submat(rectangle);
+            cropTable = source.submat(rectangle);
         } catch (Exception ex) {
             log.warn("can't crop image");
         }
-
         String fileResult = "destination.png";
-        Imgcodecs.imwrite(fileResult, sub);
+        Imgcodecs.imwrite(fileResult, cropTable);
 
         return fileResult;
+    }
+
+    private static Rect createRect(double x1, double y1, double x2, double y2, double y3) {
+        double width = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+        double height = Math.sqrt(Math.pow((y3 - y2), 2));
+        return new Rect((int) x1, (int) y3, (int) width, (int) height);
     }
 }
