621941467053024949e49b890703249924f2cf41
alex625144 <benechka1@gmail.com>
2023-05-18 17:44:15 +0300

    commit

--- src/main/java/com/parsing/exception/PDFParsingException.java
+++ src/main/java/com/parsing/exception/PDFParsingException.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-public class PDFParsingException extends RuntimeException{
+public class PDFParsingException extends RuntimeException {
 
-    public PDFParsingException (String message) {
+    public PDFParsingException(String message) {
         super(message);
     }
 }
--- src/main/java/com/parsing/rozetka/RozetkaParserService.java
+++ src/main/java/com/parsing/rozetka/RozetkaParserService.java
@@ -1,7 +1,12 @@
 package com.parsing.rozetka;
 
 
-import com.parsing.model.*;
+import com.parsing.model.LaptopItem;
+import com.parsing.model.LotResult;
+import com.parsing.model.ResultReport;
+import com.parsing.model.ResultReportItem;
+import com.parsing.model.RozetkaParsingReport;
+import com.parsing.model.Status;
 import com.parsing.repository.LotResultRepository;
 import com.parsing.repository.ResultReportRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,23 +23,22 @@ import java.util.stream.Collectors;
 @Service
 public class RozetkaParserService {
 
+    private static final long TEN_MINUTES = 600000L;
     private final RozetkaParser rozetkaParser;
     private final LotResultRepository lotResultRepository;
     private final ResultReportRepository resultReportRepository;
-
-    private static final long TEN_MINUTES = 600000L;
 
     public String findPriceByModel(String model) {
         return rozetkaParser.searchPriceByModel(model).toString();
     }
 
     @Scheduled(initialDelay = TEN_MINUTES, fixedDelay = TEN_MINUTES)
-    public void scheduledParsing(){
+    public void scheduledParsing() {
         List<LotResult> lotResults = lotResultRepository.findAllByStatus(Status.PDF_SUCCESSFULL);
 
-        for(LotResult lotResult : lotResults) {
+        for (LotResult lotResult : lotResults) {
             List<RozetkaParsingReport> rozetkaParsingReports = rozetkaParser.parseByLotResult(lotResult);
-            if(lotResult.getStatus() == Status.ROZETKA_FAILED) continue;
+            if (lotResult.getStatus() == Status.ROZETKA_FAILED) continue;
 
             BigDecimal totalPriceViolation = new BigDecimal(0);
             List<ResultReportItem> resultReportItems = new ArrayList<>(rozetkaParsingReports.size());
@@ -42,7 +46,7 @@ public class RozetkaParserService {
 
             Map<String, LaptopItem> laptopsByModel = lotResult.getLotPDFResult().getLaptopItems().stream()
                     .collect(Collectors.toMap(LaptopItem::getModel, x -> x));
-            for(RozetkaParsingReport rozetkaParsingReport : rozetkaParsingReports) {
+            for (RozetkaParsingReport rozetkaParsingReport : rozetkaParsingReports) {
                 LaptopItem laptopItem = laptopsByModel.get(rozetkaParsingReport.getModel());
 
                 ResultReportItem reportItem = new ResultReportItem();
